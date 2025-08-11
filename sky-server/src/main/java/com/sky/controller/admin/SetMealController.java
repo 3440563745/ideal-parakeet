894f4b3a9bfe8c467c75f7c079c32fe39aa433bc
hanke_sudan <sudan@hunktimes.com>
2025-08-11 17:28:43 +0800

    缓存套餐代码开发

--- sky-server/src/main/java/com/sky/controller/admin/SetMealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetMealController.java
@@ -10,6 +10,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,6 +30,7 @@ public class SetMealController {
      */
     @PostMapping
     @ApiOperation("新增套餐")
+    @CacheEvict(cacheNames ="setmealCache",key="#setmealDTO.categoryId")
         public Result Save(@RequestBody SetmealDTO setmealDTO){
             log.info("新增套餐{}",setmealDTO);
             setmealService.save(setmealDTO);
@@ -43,6 +45,7 @@ public class SetMealController {
     }
     @DeleteMapping
     @ApiOperation("套餐批量删除")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result delete(@RequestParam List<Long> ids){
       setmealService.deleteBatch(ids);
       return Result.success();
@@ -55,6 +58,7 @@ public class SetMealController {
     }
     @PutMapping
     @ApiOperation("修改套餐")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result update(@RequestBody SetmealDTO setmealDTO){
         log.info("修改套餐{}",setmealDTO);
         setmealService.update(setmealDTO);
@@ -62,6 +66,7 @@ public class SetMealController {
     }
     @PostMapping("/status/{status}")
     @ApiOperation("起售、停售套餐")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result startOrStop(@PathVariable Integer status,Long id){
         setmealService.startOrStop(status,id);
         return Result.success();
--- sky-server/src/main/java/com/sky/controller/user/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/user/SetmealController.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,7 @@ public class SetmealController {
     private SetmealService setmealService;
     @GetMapping("list")
     @ApiOperation("查询分类下的套餐")
+    @Cacheable(cacheNames = "setmealCache",key = "#categoryId")
     public Result<List<Setmeal>> list(Long categoryId){
         Setmeal setmeal=new Setmeal();
         setmeal.setCategoryId(categoryId);
