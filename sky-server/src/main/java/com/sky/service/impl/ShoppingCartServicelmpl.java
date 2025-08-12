97c6035abc79ebd8cadce2920b1d808d904cf762
hanke_sudan <sudan@hunktimes.com>
2025-08-12 15:16:32 +0800

    查看购物车代码开发

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServicelmpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServicelmpl.java
@@ -58,4 +58,14 @@ public class ShoppingCartServicelmpl implements ShoppingCartService {
             shoppingCartMapper.insert(shoppingCart);
         }
     }
+
+    @Override
+    public List<ShoppingCart> showShoppingCart() {
+        Long userId=BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = ShoppingCart.builder()
+                .userId(userId)
+                .build();
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+        return list;
+    }
 }
