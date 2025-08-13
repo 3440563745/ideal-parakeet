9f10a6d4f0377a71a7688021c042cb65686125a2
hanke_sudan <sudan@hunktimes.com>
2025-08-14 00:46:10 +0800

    订单状态定时处理

---
+++ sky-server/src/main/java/com/sky/Task/OrderTask.java
@@ -0,0 +1,47 @@
+package com.sky.Task;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@Slf4j
+public class OrderTask {
+    @Autowired
+    private OrderMapper orderMapper;
+    @Scheduled(cron = "0 * * * * ?")
+//@Scheduled(cron = "1/5 * * * * ?")
+public void processTimeoutOrder(){
+     log.info("处理支付超时订单:{}", LocalDateTime.now());
+     LocalDateTime time=LocalDateTime.now().plusMinutes(-15);
+        List<Orders> orderList=orderMapper.getByStatusAndOrderTimeLT(Orders.PENDING_PAYMENT,time);
+        if(orderList!=null&& !orderList.isEmpty()){
+            for(Orders orders:orderList){
+                orders.setStatus(Orders.CANCELLED);
+                orders.setCancelReason("订单超时，自动取消");
+                orders.setCancelTime(LocalDateTime.now());
+                orderMapper.update(orders);
+            }
+        }
+    }
+    @Scheduled(cron = "0 0 1 * * ?")
+//@Scheduled(cron = "0/5 * * * * ?")
+    public  void processDeliveryOrder(){
+        log.info("处理处于待派单状态的订单:{}",LocalDateTime.now());
+        LocalDateTime time=LocalDateTime.now().plusMinutes(-60);
+        List<Orders> orderList=orderMapper.getByStatusAndOrderTimeLT(Orders.DELIVERY_IN_PROGRESS,time);
+        if(orderList!=null&& !orderList.isEmpty()){
+            for(Orders orders:orderList){
+                orders.setStatus(Orders.COMPLETED);
+                orderMapper.update(orders);
+            }
+        }
+    }
+}
--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -3,6 +3,10 @@ package com.sky.mapper;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.core.annotation.Order;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Mapper
 public interface OrderMapper {
@@ -20,6 +24,7 @@ public interface OrderMapper {
      * @param orders
      */
     void update(Orders orders);
-
+    @Select("select * from orders where status=#{status} and order_time<#{orderTime}")
+    List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
 
 }
